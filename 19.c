02cc04f9644d7ad915a68c50f3a316940bec735d
notherland <notherland@ya.ru>
2020-12-04 10:52:33 +0300

    Fixed

--- 19.c
+++ 19.c
@@ -23,16 +23,26 @@ int main(){
 	pthread_t threads[5];
 	int progress = 0;
 
-	pthread_mutex_init (&mutex, NULL);
+	if (pthread_mutex_init (&mutex, NULL) != 0)
+	{
+		perror ("pthread_mutex_init");
+		exit(0);
+	}
 	
 	for (int i = 0; i < 5; i++){
+		if (pthread_create(&threads[i], NULL, increase, &i) != 0)
+		{
+			perror ("pthread_create");
+			continue;
+		}
 		printf ("PROCESS %d CREATED\n", i);
-		pthread_create(&threads[i], NULL, increase, &i);
 	}
 
 	for (int i = 0; i < 5; i++)
     {
-    	pthread_join(threads[i], NULL);
+    	if (pthread_join(threads[i], NULL) != 0){
+    		perror ("pthread_join");
+    	}
     }
 
     pthread_mutex_destroy(&mutex);
