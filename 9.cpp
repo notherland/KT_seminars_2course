20c8f2d283d61d103e4624f38a98fd2330318f26
notherland <notherland@ya.ru>
2020-12-07 23:39:48 +0300

    Changed statfs no statvfs

--- 9.cpp
+++ 9.cpp
@@ -1,32 +1,32 @@
-#include <sys/statfs.h>
+#include <sys/statvfs.h>
 #include <iostream>
 #include <unistd.h>
 
 using namespace std;
 
-const long Gb = 1073741824;
+const long GB = (1 << 30);
 
 int main (int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc != 2)
 	{
-		cout << "Too few arguments";
+		cout << "Usage: " << argv[0] << " file_name\n" << endl;
 		return 0;
 	}
 
-	struct statfs st;
+	struct statvfs st;
 
-	if (statfs (argv[1], &st) != 0)
+	if (statvfs (argv[1], &st) != 0)
 	{
-		perror ("statfs");
-		return 0;
+		perror ("statvfs");
+		return EXIT_FAILURE;
 	}
 
 	long block_size = st.f_bsize;
 	cout << "block size " << block_size << endl;
-	cout << "total data blocks in file system " << st.f_blocks << "; total space" << st.f_blocks * block_size / Gb << endl;
-	cout << "free blocks in fs " << st.f_bfree << "; free space " << st.f_bfree * block_size / Gb << endl;
-	cout << "free blocks availible " << st.f_bavail << "; availible space " << st.f_bfree * block_size / Gb<< endl;
+	cout << "total data blocks in file system " << st.f_blocks << "; total space " << st.f_blocks * block_size / GB << endl;
+	cout << "free blocks in fs " << st.f_bfree << "; free space " << st.f_bfree * block_size / GB << endl;
+	cout << "free blocks availible " << st.f_bavail << "; availible space " << st.f_bfree * block_size / GB<< endl;
 	
 	return 0;
 }
