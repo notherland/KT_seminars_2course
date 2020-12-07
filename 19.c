abfbeec3e5bbab63be62bf93bf98f841202f3707
notherland <notherland@ya.ru>
2020-12-07 23:33:45 +0300

    Fixed

--- 19.c
+++ 19.c
@@ -3,51 +3,51 @@
 #include <stdlib.h>
 #include <mcheck.h>
 #include <stdio.h>
+#include <time.h>
 
-int shar = 0;
-pthread_mutex_t mutex;
+volatile int shar = 0;
+const int NUM_THREADS = 5;
 
 void *increase(void *arg){
-	int i = *((int *)arg);
+	 pthread_mutex_t mutex = *((pthread_mutex_t *)arg);
+	 struct timespec time = {0, 2000000};
 	while (shar < 100){
 		pthread_mutex_lock(&mutex);
-		printf ("Process %d  %d\n", i, shar);
 		shar++;
 		pthread_mutex_unlock(&mutex);
-		sleep(0.02);
+		nanosleep(&time, NULL);
 	}
-	return NULL;
 }
 
 int main(){
-	pthread_t threads[5];
-	int progress = 0;
-
-	if (pthread_mutex_init (&mutex, NULL) != 0)
-	{
-		perror ("pthread_mutex_init");
-		exit(0);
-	}
+	pthread_t threads[NUM_THREADS];
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	struct timespec time = {0, 2000000};
+	int real_num_threads = 0;
 	
-	for (int i = 0; i < 5; i++){
-		if (pthread_create(&threads[i], NULL, increase, &i) != 0)
-		{
+	for (real_num_threads = 0; real_num_threads < NUM_THREADS; real_num_threads++){
+		if (pthread_create(&threads[real_num_threads], NULL, increase, &mutex) != 0){
 			perror ("pthread_create");
-			continue;
+			real_num_threads--;
+			break;
 		}
-		printf ("PROCESS %d CREATED\n", i);
+		printf ("THREAD %d CREATED\n", real_num_threads);
 	}
 
-	for (int i = 0; i < 5; i++)
+	while (shar < 100){
+		printf ("%d\n", shar);
+		nanosleep(&time, NULL);
+	}
+
+	for (int k = 0; k < real_num_threads; k++)
 	{
-		if (pthread_join(threads[i], NULL) != 0){
+		if (pthread_join(threads[k], NULL) != 0){
 			perror ("pthread_join");
 		}
+		printf ("THREAD %d ENDED\n", k);
 	}
 
 	pthread_mutex_destroy(&mutex);
-
-
 
 	return 0;	
 }
